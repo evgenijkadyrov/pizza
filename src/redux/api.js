db5134dc6077843acfa3e83832fb673b6242aba9
Yauheni Kadyrau <7077634@gmail.com>
2023-10-13 18:01:03 +0300

    fix hashRouter

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const pizzasAPI = {
     getPizzas(activeCategory,activeSortBy) {
-        return axios.get(`?${activeCategory!==null?`category=${activeCategory}`:''}&_sort=${activeSortBy.type}&_order=${activeSortBy.order}`)
+        return axios.get(`pizzas?${activeCategory!==null?`category=${activeCategory}`:''}&_sort=${activeSortBy.type}&_order=${activeSortBy.order}`)
     }
+
 }
