0cea7182cc0d2718fb34ce5517414713960a7ded
Yauheni Kadyrau <7077634@gmail.com>
2023-10-16 09:44:37 +0300

    fix server

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const pizzasAPI = {
     getPizzas(activeCategory,activeSortBy) {
-        return axios.get(`pizzas?${activeCategory!==null?`category=${activeCategory}`:''}&_sort=${activeSortBy.type}&_order=${activeSortBy.order}`)
+        return axios.get(`my-json-server.typicode.com/evgenijkadyrov/pizza/pizzas?${activeCategory!==null?`category=${activeCategory}`:''}&_sort=${activeSortBy.type}&_order=${activeSortBy.order}`)
     }
 
 }
